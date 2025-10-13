6b1e0f80ec5394d9f8c4f3f4003646a134b448eb
healthlaneph <healthlaneph@gmail.com>
2025-10-14 00:52:33 +0800

    cleanup: remove legacy package-lock.json

--- packages/shared/eslint.config.mjs
+++ packages/shared/eslint.config.mjs
@@ -1,4 +1,17 @@
-import { config } from "@repo/eslint-config/react-internal";
-
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default {
+  root: true,
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
