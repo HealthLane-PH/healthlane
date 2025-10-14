3ff398823d2be3064b11f751eca55c3b5b7dfdb2
healthlaneph <healthlaneph@gmail.com>
2025-10-15 05:25:08 +0800

    Fix: make /auth/signup a client only page

--- apps/web/src/app/auth/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-<p className="text-center text-sm text-gray-500 pt-2">
-  Are you a healthcare provider?{" "}
-  <a
-    href="/auth/doctors/signup"
-    className="text-[#1bae69] hover:underline"
-  >
-    Sign up as a partner
-  </a>
-</p>
